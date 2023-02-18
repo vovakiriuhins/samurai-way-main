29d135d537ab0d30a3f0cd2c7a064ea78a8c376b
vovakiriuhins <vova11999977@gmail.com>
2023-02-18 18:12:06 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <div>One div</div>
+            <div className="App">
+                <ul>
+                    <li>html</li>
+                    <li>css</li>
+                    <li>js</li>
+                    <li>React</li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
