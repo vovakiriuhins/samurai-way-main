43fc69b656377cd8d4ac68411cb170042ad0bec0
vovakiriuhins <vova11999977@gmail.com>
2023-02-19 17:32:36 +0300

    move technologies component to its new folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./Components/Technologies/Technologies";
 
 function App() {
     return (
         <div>
-            <div>One div</div>
-            <div className="App">
-                <ul>
-                    <li>html</li>
-                    <li>css</li>
-                    <li>js</li>
-                    <li>React</li>
-                </ul>
-            </div>
+            <Header/>
+            <Technologies/>
         </div>
     );
 }
+
+
+function Header() {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News Feed</a>
+            <a href="#">Messages</a>
+        </div>
+    )
+}
+
+
+
 
 export default App;
---
+++ src/Components/Technologies/Technologies.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>js</li>
+                <li>React</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Technologies;
