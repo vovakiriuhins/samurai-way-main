d620a2a3a9a24a83fae2af10f89cb31ad561e105
vovakiriuhins <vova11999977@gmail.com>
2023-02-20 16:56:45 +0300

    commit nothing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Components/Technologies/Technologies";
 import Header from "./Components/Header/Header";
+import Technologies from "./Components/Technologies/Technologies";
 
 function App() {
     return (
