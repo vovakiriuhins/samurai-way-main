849241e2675a902163bae0d58dad83b248fac988
vovakiriuhins <vova11999977@gmail.com>
2023-03-06 16:56:07 +0300

    dropping components and using props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./components/Header";
-import {Navbar} from "./components/Navbar";
-import {Profile} from "./components/Profile";
+import {Header} from "./components/Header/Header";
+import {Navbar} from "./components/Navbar/Navbar";
+import {Profile} from "./components/Profile/Profile";
 
 
-function App() {
+function App(): JSX.Element {
     return (
         <div className={"app-writer"}>
             <Header/>
